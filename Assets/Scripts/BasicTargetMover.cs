c02a56b039d531c6bb2e0477b95117490a709ee4
Ahmed mostafa <Ahmed mostafa>
2016-05-26 14:58:31 +0200

    1st commit
    Signed-off-by: Ahmed mostafa <Ahmed mostafa>

---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicTargetMover : MonoBehaviour {
+
+	public enum motionDirections{Spin, Horizontal, Vertical};
+	public motionDirections motionState=motionDirections.Horizontal;
+
+
+	public float spinSpeed = 180f, motionMagnitude = 0.1f;
+	// Update is called once per frame
+	void Update () {
+
+
+		switch (motionState) {
+		case motionDirections.Spin:
+			gameObject.transform.Rotate (Vector3.up * spinSpeed * Time.deltaTime);
+			break;
+		case motionDirections.Horizontal:
+			gameObject.transform.Translate (Vector3.right * Mathf.Cos (Time.timeSinceLevelLoad) * motionMagnitude);
+			break;
+		case motionDirections.Vertical:
+			gameObject.transform.Translate (Vector3.up * Mathf.Cos (Time.timeSinceLevelLoad) * motionMagnitude);
+			break;
+		}
+				
+	}
+}
